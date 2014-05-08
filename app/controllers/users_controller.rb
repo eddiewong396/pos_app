78d88a222a1f00f6a130119f26246bd2b2dc6595
Eddie Wong <isp396@gmail.com>
2014-05-08 21:18:52 +0800

    Reorganized files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,22 @@
-class UsersController < ApplicationController
+  class UsersController < ApplicationController
 
-	before_filter :save_login_state, :only => [:new, :create]
+  before_filter :save_login_state, :only => [:new, :create]
 
-	def new
-		#Signup Form
-		@user = User.new     
-	end
+  def new
+      #Signup Form
+      @user = User.new     
+  end
 
-	def create
-		@user = User.new(params[:user])
-		if @user.save
-			flash[:notice] = "You Signed up successfully"
-			flash[:color]= "valid"
-		else
-			flash[:notice] = "Form is invalid"
-			flash[:color]= "invalid"
-		end
-		render "new"
-	end
-	
-	private
-	
-	def users_params
-		params.require(:user).permit(:username, :email, :password, :password_confirmation)
-	end
+   def create
+    	@user = User.new(params[:user])
+    	if @user.save
+    		flash[:notice] = "You Signed up successfully"
+        flash[:color]= "valid"
+      else
+        flash[:notice] = "Form is invalid"
+        flash[:color]= "invalid"
+      end
+      render "new"
+    end
 
 end
