7d97aaa3d597d0bfcaa578f5416095ab74fcd2db
Eddie Wong <isp396@gmail.com>
2014-05-08 21:44:38 +0800

    Edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 UserAuth::Application.routes.draw do
 
-  match ':controller(/:action(/:id))(.:format)'
+  get ':controller(/:action(/:id))(.:format)'
   root :to => 'sessions#login'
-  match "signup", :to => "users#new"
-  match "login", :to => "sessions#login"
-  match "logout", :to => "sessions#logout"
-  match "home", :to => "sessions#home"
-  match "profile", :to => "sessions#profile"
-  match "setting", :to => "sessions#setting"
+  get "signup", :to => "users#new"
+  get "login", :to => "sessions#login"
+  get "logout", :to => "sessions#logout"
+  get "home", :to => "sessions#home"
+  get "profile", :to => "sessions#profile"
+  get "setting", :to => "sessions#setting"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
