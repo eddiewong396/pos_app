840acb24d5e338ab589b66993f5e581d16817a2c
Eddie Wong <isp396@gmail.com>
2014-05-08 20:43:23 +0800

    Added user controller with view files

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(:version => 201405082035) do
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+  end
+
+end
