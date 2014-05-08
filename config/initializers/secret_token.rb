a6203c8531da58b74f5bf7ea45f834fcff879a9c
Eddie Wong <isp396@gmail.com>
2014-05-08 19:47:52 +0800

    First build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PosApp::Application.config.secret_key_base = '98fe97b0a243cb0620802059cd42ecf05ee31f3d9e26128373cdf348888fe7278bc6557cfdedcacb48ccac7fcc4d68be3fc5f6398fab2b71e5aeb4efec6ad58c'
